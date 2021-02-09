67840a63f44860302592e6446c2bbd34b8fa3f1f
Joshua Luppes <cooljoebob64@gmail.com>
2021-02-09 16:56:58 -0600

    Add validation to Username field

--- src/main/java/com/tts/subscriberList/subscriber/SubscriberController.java
+++ src/main/java/com/tts/subscriberList/subscriber/SubscriberController.java
@@ -4,44 +4,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.swing.text.View;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
-public class SubscriberController {
-
-//    @Autowired
-//    private SubscriberRepository subscriberRepository;
+public class SubscriberController implements WebMvcConfigurer {
 
     @Autowired
     private SubscriberService service;
 
-    @GetMapping(value="/")
-    public String index (Subscriber subscriber){
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/result").setViewName("subscriber/result");
+    }
+
+    @GetMapping(value = "/")
+    public String index(Subscriber subscriber) {
         // Where we want to go when our application is started.
         return "subscriber/index";
     }
-    @PostMapping(value="/")
-    public String addNewSubscriber(Subscriber subscriber, Model model){
-        service.save(new Subscriber(subscriber.getFirstName(),subscriber.getLastName(),
-                subscriber.getUserName(), subscriber.getSignedUp()));
-        model.addAttribute("firstName", subscriber.getFirstName());
-        model.addAttribute("lastName", subscriber.getLastName());
-        model.addAttribute("userName", subscriber.getUserName());
-        return "subscriber/result";
+
+    @PostMapping(value = "/")
+    public String addNewSubscriber(@Valid Subscriber subscriber, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "subscriber/index";
+        } else {
+            service.save(new Subscriber(subscriber.getFirstName(), subscriber.getLastName(),
+                    subscriber.getUserName(), subscriber.getSignedUp()));
+            return "redirect:/result";
+        }
     }
 
-    @GetMapping(value="/list")
-    public String listAllSubscribers(Model model){
+    @GetMapping(value = "/list")
+    public String listAllSubscribers(Model model) {
         List<Subscriber> listSubscribers = service.listAll();
         model.addAttribute("listSubscribers", listSubscribers);
 
         return "subscriber/searchResults";
     }
-    @GetMapping(value="/q")
-    public String searchResults(Model model, @Param("keyword")String keyword){
+
+    @GetMapping(value = "/q")
+    public String searchResults(Model model, @Param("keyword") String keyword) {
         List<Subscriber> listSubscribers = service.searchByKeyword(keyword);
         model.addAttribute("listSubscribers", listSubscribers);
         model.addAttribute("keyword", keyword);
@@ -49,11 +59,10 @@ public class SubscriberController {
         return "subscriber/searchResults";
     }
 
-    @GetMapping(value="/search")
-    public String searchPage(){
+    @GetMapping(value = "/search")
+    public String searchPage() {
         return "subscriber/searchEntry";
     }
-
 
 
 }
